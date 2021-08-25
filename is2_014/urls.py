8a411df88cae9a60f39e0452261b3a10996afca4
Martin Aponte <martin.aponte8@gmail.com>
2021-08-25 17:58:23 -0400

    cambios en el menu 1.2

--- is2_014/urls.py
+++ is2_014/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('index/', TemplateView.as_view(template_name="index.html")),
     path('accounts/', include('allauth.urls')),
     path('index/logout', LogoutView.as_view()),
+    path('logout', LogoutView.as_view()),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
