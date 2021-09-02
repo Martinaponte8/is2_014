1e34611fa50c274cd1eca44a1e81a9fc750be623
Martin Aponte <martin.aponte8@gmail.com>
2021-09-02 15:35:59 -0400

    funcion de eliminar rol

--- rol/urls.py
+++ rol/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from .views import eliminar3
 # from django.contrib.auth.views import login
 from .models import *
 
@@ -13,4 +14,5 @@ urlpatterns = [
 	url(r'^$', views.RolListView.as_view(),name='rol_list'),
 	url(r'^create/$', views.CreateRolView.as_view(), name='create_rol'),
 	path(route='update_rol/<int:pk>/', view=views.UpdateRolView.as_view(), name='update_rol'),
+	path("eliminar/<int:rol_id>/", eliminar3, name="eliminar3"),
 ]
