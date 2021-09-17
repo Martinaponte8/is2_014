3cba31b19973414a04f4693fdbcee9ffbc2654b0
Martin Aponte <martin.aponte8@gmail.com>
2021-09-17 18:14:51 -0400

    cambios en settings.py

--- is2_014/settings.py
+++ is2_014/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 
 
 #esto comente
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
