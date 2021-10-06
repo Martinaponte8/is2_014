a52307c3f76c6fdff6f908874c2e2ec9a52495c0
Martin Aponte <martin.aponte8@gmail.com>
2021-10-06 16:27:46 -0300

    cambios en permisos y agrego comentarios

--- proyecto/models.py
+++ proyecto/models.py
@@ -1,4 +1,4 @@
-import datetime
+
 from django.db import models
 from rol.models import Rol
 
@@ -15,6 +15,10 @@ ESTADOS_PROYECTO = (
 )
 
 class Proyecto(models.Model):
+    """
+        Implementa la clase de Proyectos, almacena datos generales acerca del proyecto:
+        fecha de inicio, fecha fin, nombre, estado y descripcion
+        """
 
     nombre = models.CharField(max_length=20, blank=False, null=False)
     fecha_inicio = models.DateField(blank=True, null=True)
@@ -25,9 +29,19 @@ class Proyecto(models.Model):
     fecha_fin_estimada = models.DateField('Fecha de Fin Estimada', blank=False, null=False)
 
     def __str__(self):
+        """
+                Metodo que retorna el nombre del proyecto actual
+                :return: retorna el valor del campo nombre del objeto actual
+                """
+
         return self.nombre
 
 class TeamMember(models.Model):
+    """
+        Implementa la clase de Team Member, el cual es una combinacion de un usuario y un rol
+        ligada a un proyecto
+        """
+
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, null=False, blank=False)
     usuario = models.ForeignKey('usuarios.Usuario', on_delete=models.PROTECT, blank=False, null=False)
     rol = models.ForeignKey(Rol, on_delete=models.PROTECT, blank=False, null=False)
