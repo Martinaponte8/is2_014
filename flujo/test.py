97cca3980787ee35835ea5737fef1c422f9a18a1
Emanuel6282 <89362050+emanuel6282@users.noreply.github.com>
2021-10-09 14:10:48 -0300

    test flujo

--- flujo/test.py
+++ flujo/test.py
@@ -1,2 +1,21 @@
-from django.test import TestCase
+import unittest
+from flujo.views import TableroTemplateView
+from proyecto.models import Proyecto
+from flujo.models import Flujo
+
+class Test(unittest.TestCase):
+
+    def test_creacion_flujo(self):
+        flujo = Flujo()
+        self.assertIsNotNone(flujo)
+
+    def test_flujo_propio_de_proyecto(self):
+        proyecto1 = Proyecto()
+        flujo = Flujo(proyecto=proyecto1, nombre="proyecto1", descripcion="descripcion")
+        self.assertEquals(proyecto1, flujo.proyecto)
+
+    def test_tablero(self):
+        tablero = TableroTemplateView()
+        self.assertIsNotNone(tablero)
+
 
