060c4c5cb52f67168548c6820cd199cfd1c6452a
jorge <jorgemi2411@fpuna.edu.py>
2021-10-09 14:11:50 -0300

    test usuarios

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,21 +1,21 @@
-# from django.test import TestCase
-# import unittest
-# from usuarios.models import Usuario
-#
-# # Create your tests here.
-#
-# class Test(unittest.TestCase):
-#
-#     """
-#     Test de creacion de Usuario sin nombre
-#     """
-#     def test_nombre(self):
-#         nombre = Usuario().first_name
-#         self.assertIsNotNone(nombre)
-#
-#     """
-#         Test de creacion de Usuario sin apellido
-#     """
-#     def test_apellido(self):
-#         apellido = Usuario().last_name
-#         self.assertIsNotNone(apellido)
+from django.test import TestCase
+import unittest
+from usuarios.models import Usuario
+
+# Create your tests here.
+
+class Test(unittest.TestCase):
+
+    """
+    Test de creacion de Usuario sin nombre
+    """
+    def test_nombre(self):
+        nombre = Usuario().first_name
+        self.assertIsNotNone(nombre)
+
+    """
+        Test de creacion de Usuario sin apellido
+    """
+    def test_apellido(self):
+        apellido = Usuario().last_name
+        self.assertIsNotNone(apellido)
